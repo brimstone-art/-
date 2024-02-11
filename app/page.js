8a58b8996092e4982b46611c58a79ab3da8537c5
brimstone-art <149525088+brimstone-art@users.noreply.github.com>
2024-02-11 19:46:27 +0300

    коммит

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { Banner } from "./components/Banner/Banner"; 
+import { PopularCardsList } from "./components/CardsList/PopularCardsList"; 
+import { NewCardsList } from "./components/CardsList/NewCardsList"; 
+import { Promo } from "./components/Promo/Promo"; 
+
+export default function Home() {
+  return (
+   <main className="main">
+    <Banner/>
+    <PopularCardsList/>
+    <NewCardsList/>
+    <Promo/>
+  </main>
+  );
+}
